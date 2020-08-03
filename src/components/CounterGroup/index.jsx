3def8fc7cc7891551a402e3f04b98711409a322f
ZHOUGR2 <286348794@qq.com>
2020-08-03 16:20:59 +0800

    [React] [Green] feature:implement input number generate a counter group

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -4,11 +4,32 @@ class CounterGroup extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            groupNum: this.props.group
+            groupNum: 0
         }
     }
+    shouldComponentUpdate(){
+        return true;
+    }
+    onChange(e){
+        var num = e.target.value.length>0?e.target.value:0;
+        this.state.groupNum = parseInt(num);
+    }
+    getInput = ()=>{
+        this.setState({
+            groupNum:  this.state.groupNum 
+        });
+    }
     render() {
-        return (new Array(6).fill(0).map((key, index) => <Counter/>));
+        return (
+            <div>
+                <input placeholder="input a number"
+                    onChange={this.onChange.bind(this)}/>
+                <button onClick={this.getInput} >生成</button>
+                <div>
+                    {new Array(this.state.groupNum).fill(0).map((key, index) => <Counter key={index}/>)}
+                </div>
+            </div>
+        );
     }
 }
 export default CounterGroup
